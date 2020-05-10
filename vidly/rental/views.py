7112857406c0006dac2b37d11ba9d771185b7483
edpinck03w <edward.pinckney@gmail.com>
2020-05-10 13:44:39 -0400

    Class 112 Session 3

--- vidly/rental/views.py
+++ vidly/rental/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 #MVC python manage.py runserver
 
 #defining an endpoint
 def index(request):
-    return HttpResponse("Hello World!!!")
+    all_movies = Movie.objects.all() # read Movie table to a list
+    return render(request,'index.html', { 'title': 'Movies Catalog', 'movies': all_movies })
+    #return HttpResponse("Hello World!!!")
 
 
 #about send your name
@@ -14,4 +17,5 @@ def about(request):
     return HttpResponse("Edward Pinckney")
 
 def soon(request):
-    return HttpResponse("Page coming soon, stay in touch :)")
+    return render(request, 'comingSoon.html')
+    #return HttpResponse("Page coming soon, stay in touch :)")
